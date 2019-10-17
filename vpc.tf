573a72574a32d611c468fb063b4034b7e955dfc4
Jijo <jgeorg0001@gmail.com>
2019-10-17 06:21:57 +0530

    completed vpc

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main"{
   cidr_block     = "10.10.0.0/16"
   enable_dns_support = "true"
   enable_dns_hostnames = "true"
-  tags {
+  tags = {
     Name = "myapp-vpc"
  }
 }
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet_1"{
    cidr_block = "10.10.1.0/24"
    availability_zone = "eu-west-3-1a"
    map_public_ip_on_launch = "true"
-   tags {
+   tags = {
      Name = "${var.PROJECT_NAME}-vpc-public-subnet-1"
  }
 
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_subnet_2"{
    cidr_block = "10.10.2.0/24"
    availability_zone = "eu-west-3-1b"
    map_public_ip_on_launch = "true"
-   tags {
+   tags = {
      Name = "${var.PROJECT_NAME}-vpc-public-subnet-2"
  }
 
@@ -43,7 +43,7 @@ resource "aws_subnet" "private_subnet_1"{
    cidr_block = "10.10.3.0/24"
    availability_zone = "eu-west-3-1a"
    map_public_ip_on_launch = "true"
-   tags {
+   tags = {
      Name = "${var.PROJECT_NAME}-vpc-private-subnet-1"
  }
 }
@@ -54,7 +54,7 @@ resource "aws_subnet" "private_subnet_2"{
    cidr_block = "10.10.4.0/24"
    availability_zone = "eu-west-3-1b"
    map_public_ip_on_launch = "true"
-   tags {
+   tags = {
      Name = "${var.PROJECT_NAME}-vpc-private-subnet-2"
  }
 }
@@ -62,7 +62,7 @@ resource "aws_subnet" "private_subnet_2"{
 #Aws IGW
 resource "aws_internet_gateway" "igw"{
    vpc_id = "${aws_vpc.main.id}"
-   tags {
+   tags = {
      Name = "${var.PROJECT_NAME}-vpc-internet-gateway"
  }
 
@@ -75,12 +75,13 @@ resource "aws_eip" "nat_eip" {
 }
 
 #NAT GW
-resource "aws_nat_gateway" "ngw"{
+resource "aws_nat_gateway" "ngw" {
    allocation_id = "${aws_eip.nat_eip.id}"
    subnet_id = "${aws_subnet.public_subnet_1.id}"
    depends_on = ["aws_internet_gateay.igw"]
    tags {
-     Name = "${var.PROJECT_NAME}-vpc-NAT-gateway"}
+     Name = "${var.PROJECT_NAME}-vpc-NAT-gateway"
+   }
 }
 
 #route table for public
@@ -91,7 +92,8 @@ resource "aws_route_table" "public"{
      gateway_id = "${aws_internet_gateway.igw.id}"
    }
    tags {
-     Name = "${var.PROJECT_NAME}-public_route_table"}
+     Name = "${var.PROJECT_NAME}-public_route_table"
+   }
  }
 
 resource "aws_route_table" "private"{
@@ -101,7 +103,8 @@ resource "aws_route_table" "private"{
      gateway_id = "${aws_nat_gateway.ngw.id}"
    }
    tags {
-     Name = "${var.PROJECT_NAME}-private_route_table"}
+     Name = "${var.PROJECT_NAME}-private_route_table"
+   }
  }
 
 #Route table association for public subnet
@@ -123,7 +126,7 @@ resource "aws_route_table_association" "to_private_subnet_1"{
 }
 
 
-resource "aws_route_table_association" "to_public_subnet_1"{
+resource "aws_route_table_association" "to_private_subnet_2"{
    subnet_id = "${aws_subnet.private_subnet_2.id}"
    route_table_id = "${aws_route_table.private.id}"
 }
